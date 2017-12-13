82ee52b9349c181721f905fae43e5d255b57efef
Michele Lapolla <mikee.lapo@gmail.com>
2017-12-13 11:39:29 +0100

    Truly fixed medium blocks of duplicated code

--- cv4j/src/main/java/com/cv4j/core/binary/Skeleton.java
+++ cv4j/src/main/java/com/cv4j/core/binary/Skeleton.java
@@ -84,46 +84,49 @@ public class Skeleton {
 			}
 		}
 
-		// update pixels and release memory
 		binary.putGray(output);
-		output = null;
-		distmap = null;
 	}
 
 	private boolean dt(byte[] input, byte[] output, int[] distmap, int level, int width, int height) {
 		boolean stop = true;
-		int p1=0;
-		int p2=0;
-		int p3=0;
-		int p4=0;
-		int p5=0; 
-		int p6=0;
-		int p7=0;
-		int p8=0;
-		int p9=0;
-		int offset = 0;
-		for(int row=1; row<height-1; row++) {
-			offset = row*width;
-			for(int col=1; col<width-1; col++) {
-				p1 = input[offset-width+col-1]&0xff;
-				p2 = input[offset-width+col]&0xff;
-				p3 = input[offset-width+col+1]&0xff;
-				p4 = input[offset+col-1]&0xff;
-				p5 = input[offset+col]&0xff;
-				p6 = input[offset+col-1]&0xff;
-				p7 = input[offset+width+col-1]&0xff;
-				p8 = input[offset+width+col]&0xff;
-				p9 = input[offset+width+col+1]&0xff;
-				int sum = p1+p2+p3+p4+p6+p7+p8+p9;
-				int total = 255*8;
+		int numOfPixels = 8;
+		int total = 255 * numOfPixels;
+
+		for(int row = 1; row < height-1; row++) {
+			int offset = row * width;
+			for(int col = 1; col < width-1; col++) {
+				int p5 = input[offset+col] & andValue;
+				int sum = sumInputValues(input, row, col);
+				
 				if(p5 == 255 &&  sum != total) {
-					output[offset+col] = (byte)0;
-					distmap[offset+col] = distmap[offset+col] + level;
+					output[offset + col] = (byte) 0;
+					distmap[offset + col] = distmap[offset + col] + level;
 					stop = false;
 				}
 			}
 		}
+
 		return stop;
+	}
+
+
+	public int sumInputValues(int[] input, int row, int col) {
+		int offset = row * width;
+		int andValue = 0xff;
+
+		int p1 = input[offset-width+col-1] & andValue;
+		int p2 = input[offset-width+col] & andValue;
+		int p3 = input[offset-width+col+1] & andValue;
+		int p4 = input[offset+col-1] & andValue;
+		int p5 = input[offset+col] & andValue;
+		int p6 = input[offset+col-1] & andValue;
+		int p7 = input[offset+width+col-1] & andValue;
+		int p8 = input[offset+width+col] & andValue;
+		int p9 = input[offset+width+col+1] & andValue;
+
+		int sum = (p1 + p2 + p3 + p4 + p6 + p7 + p8 + p9);
+
+		return sum
 	}
 
 }
